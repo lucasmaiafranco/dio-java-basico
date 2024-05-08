c34563c6d21d13fd0f38bc7f6686ec2499d2c3ab
Lucas Franco <lucas.maiafranco@gmail.com>
2024-05-08 10:07:21 -0300

    tipos variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,14 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Variáveis podem mudar de valor
+        int numero = 5;
+        numero = 10;
+        System.out.println(numero);
+
+        // Constantes
+        final double VALOR_DE_PI = 3.14;
+        // VALOR_DE_PI = 12.2; -> Contantes não podem mudar
+        System.out.println(VALOR_DE_PI);
+
     }
 }
